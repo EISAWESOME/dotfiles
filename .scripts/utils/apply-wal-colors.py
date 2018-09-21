34a82449c9e0ff522b9067feaf1f3ca87a22cfd0
Clément S <csimonin.dev@outlook.fr>
2018-09-22 00:23:37 +0200

    Ajout de parametres pour opti

--- .scripts/utils/apply-wal-colors.py
+++ .scripts/utils/apply-wal-colors.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import re
+import sys
 import os
 import json
 from pathlib import Path
@@ -36,50 +37,53 @@ with open(home + '/.cache/wal/colors.json', 'r') as f:
 
 #region conky
 
-# Rename the original conky config to .old
-os.rename(conkyConfDir + 'conky.conf', conkyConfDir + 'conky.conf.old')
+if("conky" in sys.argv):
+    print("Updating conky colors ...")
+    # Rename the original conky config to .old
+    os.rename(conkyConfDir + 'conky.conf', conkyConfDir + 'conky.conf.old')
 
-# Open conky config file
-with open(conkyConfDir + 'conky.conf.old','rt') as fin:
+    # Open conky config file
+    with open(conkyConfDir + 'conky.conf.old','rt') as fin:
 
-    with open(conkyConfDir + 'conky.conf','wt') as fout:
-        for line in fin:
-            for i in range (0, 10):
-                # Replace colorN=#xxxxx by the color specified by wal
-                j = str(i)
-                regex = r"color" + j + "=[^,]*"
-                replace = "color" + j +"='%s'" % walcolors['color' + j]
-                # print(replace)
-                line = re.sub(regex, replace, line)
-            #print(line)
-            fout.write(line)
+        with open(conkyConfDir + 'conky.conf','wt') as fout:
+            for line in fin:
+                for i in range (0, 10):
+                    # Replace colorN=#xxxxx by the color specified by wal
+                    j = str(i)
+                    regex = r"color" + j + "=[^,]*"
+                    replace = "color" + j +"='%s'" % walcolors['color' + j]
+                    # print(replace)
+                    line = re.sub(regex, replace, line)
+                #print(line)
+                fout.write(line)
 
 #endregion
 
 #region tint2
+if ("tint2" in sys.argv):
+    print("Updating tint2 colors ...")
+    os.rename(tint2ConfDir + 'tint2rc', tint2ConfDir + 'tint2rc.old')
 
-os.rename(tint2ConfDir + 'tint2rc', tint2ConfDir + 'tint2rc.old')
+    # Open conky config file
+    with open(tint2ConfDir + 'tint2rc.old','rt') as fin:
 
-# Open conky config file
-with open(tint2ConfDir + 'tint2rc.old','rt') as fin:
+        with open(tint2ConfDir + 'tint2rc','wt') as fout:
+            write = False        
+            for line in fin:
+                # Toggle write mode when we encouter a #WAL line
+                # toggle it back when we encounter a #ENDWAL line
+                if('#WAL' in line):
+                    write = True
+                if('#ENDWAL' in line):
+                    write = False
 
-    with open(tint2ConfDir + 'tint2rc','wt') as fout:
-        write = False        
-        for line in fin:
-            # Toggle write mode when we encouter a #WAL line
-            # toggle it back when we encounter a #ENDWAL line
-            if('#WAL' in line):
-                write = True
-            if('#ENDWAL' in line):
-                write = False
-
-            if(write):
-                # Replace 'XXX_color = #XXXXXX' by the color specified by wal
-                regex = r".+_color = #.{3,6}"
-                replace = "background_color = %s" % walcolors['color2'].upper()
-                # print(replace)
-                line = re.sub(regex, replace, line)
-                print(line)
-            fout.write(line)
+                if(write):
+                    # Replace 'XXX_color = #XXXXXX' by the color specified by wal
+                    regex = r".+_color = #.{3,6}"
+                    replace = "background_color = %s" % walcolors['color5'].upper()
+                    # print(replace)
+                    line = re.sub(regex, replace, line)
+                    # print(line)
+                fout.write(line)
 
 #endregion
