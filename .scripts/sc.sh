0cde6e10d3250853b2ba4569cc337bbe8d44e4b3
Clément S <csimonin.dev@outlook.fr>
2018-09-21 20:47:20 +0200

    scripts initial

---
+++ .scripts/sc.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# Screenshot and copy to clipboard 
+
+import png:- | xclip -selection c -t image/png
---
+++ .scripts/ss.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# Screenshot and save to ~/Pictures
+
+scrot -s ~/Pictures/$(timestamp).png
